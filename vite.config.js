89a61ae353e8710b72289602820733a1e3ebcd77
Aram <hernandez.aram@gmail.com>
2023-05-25 14:23:53 +0100

    definitivo

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,8 @@ export default defineConfig({
             input: [
                 'resources/sass/app.scss',
                 'resources/js/app.js',
+                'resources/css/ordenadores.css',
+                'resources/js/buscador.js',
             ],
             refresh: true,
         }),
